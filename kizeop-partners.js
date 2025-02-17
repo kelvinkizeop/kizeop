96619f899cbc7bac889461ff8d4a620c0666409a
kelvinkizeop <bernardkelvin766@gmail.com>
2025-02-17 16:30:32 +0100

    Add files via upload

---
+++ kizeop-partners.js
@@ -0,0 +1,53 @@
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+            observer.unobserve(entry.target);
+        }
+    });
+}, { threshold: 0.5 });
+
+document.querySelectorAll('.scroll-zoom').forEach(element => {
+    observer.observe(element);
+});
+
+
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({ pageLanguage: 'en' }, 'google_translate_element');
+}
+
+//  Google Translate 
+let script = document.createElement('script');
+script.src = "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+document.head.appendChild(script);
+
+
+
+
+//menu toogle
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.getElementById("hamburger"); 
+    const sidebar = document.querySelector(".sidebar"); 
+  
+
+  
+    hamburger.addEventListener("click", () => {
+        sidebar.classList.toggle("show"); 
+    });
+
+
+    const sidebarItems = sidebar.querySelectorAll("ul li");
+    sidebarItems.forEach(item => {
+        item.addEventListener("click", () => {
+            sidebar.classList.remove("show"); 
+        });
+    });
+});
+
+//sidebar drop down toggle
+document.querySelectorAll(".sidebar-dropdown").forEach(item => {
+    item.addEventListener("click", function(event) {
+        event.stopPropagation(); // Prevents the sidebar from closing
+        this.classList.toggle("open");
+    });
+});
